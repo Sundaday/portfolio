faf89490ee5c3b06aeddf04dc52c9cfbe9cb2229
Sundaday <john.koch.pro@gmail.com>
2022-04-01 00:16:32 +0200

    add navMenu css

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavMenuStyles = styled.div`
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 1rem 0;
+  background: var(--dark-bg);
+  ul {
+    max-width: 1200px;
+    margin: 0 auto;
+    width: 90%;
+    text-align: center;
+    li {
+      display: inline-block;
+      border-radius: 8px;
+      transition: 0.3s ease background-color;
+      &:hover {
+        background-color: var(--deep-black);
+      }
+    }
+    a {
+      display: inline-block;
+      font-family: 'RobotoMono Regular';
+      padding: 1rem 1rem;
+      font-size: 2rem;
+      color: var(--gar-1);
+      outline: none;
+    }
+  }
+`;
 
 export default function NavMenu() {
   return (
-    <div>
+    <NavMenuStyles>
       <ul>
         <li>
-          <NavLink to="/Home">Home</NavLink>
+          <NavLink to="/">Home</NavLink>
         </li>
         <li>
           <NavLink to="/about">About</NavLink>
@@ -18,6 +51,6 @@ export default function NavMenu() {
           <NavLink to="/contact">Contact</NavLink>
         </li>
       </ul>
-    </div>
+    </NavMenuStyles>
   );
 }
