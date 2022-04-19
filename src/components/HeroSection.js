d9884939209181609a6662de178ebea7adf8319a
Sundaday <john.koch.pro@gmail.com>
2022-04-19 13:40:20 +0200

    init Button

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,14 +7,17 @@ export default function HeroSection() {
     <div>
       <div className="container">
         <h1 className="hero__heading">
-          <span>Bonjour, Vous êtes</span>
+          <span>Bonjour, Vous êtes </span>
           <span>sur la page de John</span>
         </h1>
         <div className="hero__img">
           <img src={HeroImg} alt="" />
         </div>
         <div className="hero__info">
-          <PText />
+          <PText>
+            Programmeur en herbe orienté backend, j'aspire à acquérir la
+            philosophie devOps.
+          </PText>
         </div>
       </div>
     </div>
