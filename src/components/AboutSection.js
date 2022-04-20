f97dce9bfc6c6a1fe39de3f72c63486439a1605d
Sundaday <john.koch.pro@gmail.com>
2022-04-21 01:12:59 +0200

    About section

---
+++ src/components/AboutSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SectionTitle from './SectionTitle';
+
+export default function AboutSection() {
+  return (
+    <div className="contairer">
+      <div className="aboutSection">
+        <SectionTitle />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/SectionTitle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SectionTitle() {
+  return (
+    <div>
+      <p>Ceci est un titre</p>
+      <h1>Ceci est une section</h1>
+    </div>
+  );
+}
