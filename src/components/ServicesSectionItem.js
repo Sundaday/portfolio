2f3187ed7503f5202f2104a15378aa0c55a926dc
Sundaday <john.koch.pro@gmail.com>
2022-04-29 19:12:54 +0200

    add Service section next part

--- src/components/ServicesSectionItem.js
+++ src/components/ServicesSectionItem.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { MdDesktopMac } from 'react-icons/all';
+import PText from './PText';
 
 export default function ServicesSectionItem() {
   return (
     <div>
-      <h1>c'est ici qu'on decris nos services !</h1>
+      <div className="servicesItem__icon">
+        <MdDesktopMac />
+        <div className="servicesItem__title">Web Design</div>
+        <PText>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+          atque consectetur culpa dolorem dolores excepturi fuga magni mollitia
+          obcaecati officiis pariatur porro possimus quam reiciendis sapiente,
+          suscipit temporibus vero voluptas?
+        </PText>
+      </div>
     </div>
   );
 }
