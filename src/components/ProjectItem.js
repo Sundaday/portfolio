a72a6475b900b0ba104ac5b02336e58e2539f891
Sundaday <john.koch.pro@gmail.com>
2022-05-07 15:56:52 +0200

    add css projectiÃmg

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import projectImg from '../assets/images/projectImg.png';
+
+const ProjectItemStyles = styled.div`
+  .projectItem__img {
+    width: 100%;
+    height: 400px;
+    overflow: hidden;
+    border-radius: 12px;
+    display: inline-block;
+    border: 3px solid var(--gray-2);
+    .img {
+      height: 100%;
+    }
+  }
+  .projectItem__info {
+    margin-top: 1rem;
+    background-color: var(--deep-dark);
+    padding: 1rem;
+    border-radius: 12px;
+  }
+  .proctItem__title {
+    font-size: 2.2rem;
+  }
+  .projectItem__desc {
+    font-size: 1.6rem;
+    font-family: 'RobotoMono Regular';
+    margin-top: 1rem;
+  }
+  @media only screen and (max-width: 768px) {
+    .projectItem__img {
+      height: 350px;
+    }
+  }
+`;
 
 export default function ProjectItem() {
   return (
-    <div>
+    <ProjectItemStyles>
       <Link to="/projects" className="projectItem__img">
         <img src={projectImg} alt="project img" />
       </Link>
@@ -19,6 +53,6 @@ export default function ProjectItem() {
           totam. Doloremque.
         </p>
       </div>
-    </div>
+    </ProjectItemStyles>
   );
 }
