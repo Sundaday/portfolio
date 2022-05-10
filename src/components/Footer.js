124d33f4e0ca047b23a4f009996fdcb802a41bd8
Sundaday <john.koch.pro@gmail.com>
2022-05-10 10:03:34 +0200

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import FooterCol from './FooterCol';
 
 const FooterStyles = styled.div`
   background-color: var(--deep-dark);
-  padding-top: 10rem;
+  padding-top: 5rem;
   .container {
     display: flex;
     gap: 3rem;
@@ -21,6 +21,35 @@ const FooterStyles = styled.div`
   .footer__col1__title {
     font-size: 3.5rem;
     margin-bottom: 1rem;
+  }
+  .copyright {
+    background-color: var(--dark-bg);
+    text-align: left;
+    padding: 1rem 0;
+    margin-top: 5rem;
+    .para {
+      margin-left: 0;
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    .container {
+      flex-direction: column;
+      gap: 0rem;
+      & > div {
+        margin-top: 5rem;
+      }
+    }
+    .footer__col1,
+    .para {
+      max-width: 100%;
+    }
+    .copyright {
+      .container {
+        div {
+          margin-top: 0;
+        }
+      }
+    }
   }
 `;
 
@@ -65,10 +94,60 @@ export default function Footer() {
           />
         </div>
         <div className="footer__col3">
-          <FooterCol />
+          <FooterCol
+            heading="Info Contact"
+            links={[
+              {
+                title: '+336 461 536 45',
+                path: 'tel:+336 461 536 45',
+              },
+              {
+                title: 'johnkochdev@gmail.com',
+                path: 'mailto:johnkochdev@gmail.com',
+              },
+              {
+                title: 'Thionville, France',
+                path: 'https://www.google.com/maps/@49.3529436,6.1781913,15z',
+              },
+            ]}
+          />
         </div>
         <div className="footer__col4">
-          <FooterCol />
+          <FooterCol
+            heading="Social links"
+            links={[
+              {
+                title: 'LinkedIn',
+                path: 'https://www.linkedin.com/in/jonathankochdev',
+              },
+              {
+                title: 'Github',
+                path: 'https://github.com/Sundaday',
+              },
+              {
+                title: 'Discord',
+                path: 'https://discord.gg/435P33bb3d',
+              },
+              {
+                title: 'Twitter',
+                path: 'https://twitter.com/John_Koch_',
+              },
+            ]}
+          />
+        </div>
+      </div>
+      <div className="copyright">
+        <div className="container">
+          <PText>
+            © 2022 - Designed by{' '}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="http://www.linkedin.com/in/jonathankochdev"
+            >
+              John Koch
+            </a>{' '}
+          </PText>
         </div>
       </div>
     </FooterStyles>
