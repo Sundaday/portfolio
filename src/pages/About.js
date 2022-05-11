ca13c22567acda98ef62efa52307c91cd9543332
Sundaday <john.koch.pro@gmail.com>
2022-05-11 16:01:12 +0200

    fixing css

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,7 @@ import PText from '../components/PText';
 import Button from '../components/Button';
 import AboutImg from '../assets/images/about-page-img.png';
 import AboutInfoItem from '../components/AboutInfoItem';
+import ContactBanner from '../components/ContactBanner';
 
 const AboutPageStyles = styled.div`
   padding: 20rem 0 10rem 0;
@@ -44,6 +45,32 @@ const AboutPageStyles = styled.div`
       border-radius: 1.6rem;
     }
   }
+  .about__info__items {
+    margin-top: 15rem;
+  }
+  .about__info__item {
+    margin-bottom: 10rem;
+  }
+  .about__info__heading {
+    font-size: 3.6rem;
+    text-transform: uppercase;
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 10rem;
+    .top-section {
+      flex-direction: column;
+      gap: 5rem;
+    }
+    .about__subheading {
+      font-size: 1.8rem;
+    }
+    .about__heading {
+      font-size: 2.8rem;
+    }
+    .about__info__heading {
+      font-size: 3rem;
+    }
+  }
 `;
 
 export default function About() {
@@ -53,9 +80,9 @@ export default function About() {
         <div className="top-section">
           <div className="left">
             <p className="about__subheading">
-              Salut, <span>Je suis Jonathan !</span>
+              Salut, Je suis <span>Jonathan</span>!
             </p>
-            <h2 className="about__heading">PROGRAMMEUR</h2>
+            <h2 className="about__heading">DEV</h2>
             <div className="about__info">
               <PText>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -72,11 +99,48 @@ export default function About() {
         </div>
         <div className="about__info__items">
           <div className="about__info__item">
-            <h1 className="about__info___heading">Education</h1>
-            <AboutInfoItem title="Education" />
+            <h1 className="about__info__heading">Expérience</h1>
+            <AboutInfoItem
+              title="07/21 - 09/21"
+              items={['Stage - RedSpher Contern - Assurance Quality']}
+            />
+          </div>
+          <div className="about__info__item">
+            <h1 className="about__info__heading">Skills</h1>
+            <AboutInfoItem
+              title="Front End"
+              items={['HTML5', 'CSS3', 'JavaScript', 'ES7+', 'React']}
+            />
+            <AboutInfoItem
+              title="Back End"
+              items={[
+                'TS',
+                'C#',
+                'ASP.NET',
+                'Node.js',
+                'PostgreSQL',
+                'MongoDB',
+              ]}
+            />
+            <AboutInfoItem
+              title="Dev Ops"
+              items={['Ubuntu', 'Bash', 'Git', 'Docker', 'Travis']}
+            />
+          </div>
+          <div className="about__info__item">
+            <h1 className="about__info__heading">Education</h1>
+            <AboutInfoItem
+              title="Formation"
+              items={['OpenClassRoom', 'AFPA']}
+            />
+            <AboutInfoItem
+              title="Université"
+              items={['Aix-Marseille Université']}
+            />
           </div>
         </div>
       </div>
+      <ContactBanner />
     </AboutPageStyles>
   );
 }
